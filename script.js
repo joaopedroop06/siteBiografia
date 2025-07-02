be410b5a0dd19d64a5a0dd6580a95e2cb2f515d5
joaopedroop06 <jpfaria.pdv@gmail.com>
2025-07-01 22:42:31 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let botao = document.querySelector("#botao");
+let resposta =document.querySelector("#mensagem")
+
+botao.addEventListener("click", e=>{
+    resposta.innerHTML = "A vida não é sobre ter apenas um destino, mas sobre aprender a navegar entre as oportunidades.";
+});
